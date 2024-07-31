e8c1ab90eee8fb6bc41a5c111eb2b08f81ae920b
juancolchete <juanudkp@gmail.com>
2024-07-31 16:31:06 -0300

    ci: proper backup mysql

--- bkpdb.sh
+++ bkpdb.sh
@@ -1,4 +1,4 @@
 source ./.env
 timestamp=$(date +%s)
-mkdir ./bkp/$DB_NAME
-mysqldump -u $DB_USER -p $DB_PASS -P $PORT -h $DB_HOST  $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
+mkdir -p ./bkp/$DB_NAME
+mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
