cf9396b1737490cf5c12dcc070bc5b2ebdd42688
juancolchete <juanudkp@gmail.com>
2024-07-31 17:06:09 -0300

    chore: prepare for multiple enviroments

--- bkpDB.sh
+++ bkpDB.sh
@@ -1,5 +1,13 @@
-source ./.env
+COMMAND="your_command"
 
-mkdir -p ./bkp/$DB_NAME
-timestamp=$(date +%s)
-mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
+# Iterate over each file in the directory
+for file in $PWD/envs/{*,.*}
+do
+    if [ -f "$file" ]; then
+      echo $file
+      source $file
+      mkdir -p ./bkp/$DB_NAME
+      timestamp=$(date +%s)
+      mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
+    fi
+done
