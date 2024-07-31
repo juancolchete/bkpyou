be2f16e48d70164e1c4fc3a387e51661f18dfe69
juancolchete <juanudkp@gmail.com>
2024-07-31 20:24:13 -0300

    chore: progress by number

--- bkpDB.sh
+++ bkpDB.sh
@@ -1,11 +1,14 @@
+i=0
+total_files=$(find "./envs" -type f | wc -l)
 for file in $PWD/envs/{*,.*}
 do
     if [ -f "$file" ]; then
-      echo "start bkp $DB_NAME"
+      ((i++))
+      echo "($i/$total_files) start bkp $DB_NAME"
       source $file
       mkdir -p ./bkp/$DB_NAME
       timestamp=$(date +%s)
       mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
-      echo "ended bkp $DB_NAME"
+      echo "($i/$total_files) ended bkp $DB_NAME"
     fi
 done
--- clearOldDB.sh
+++ clearOldDB.sh
@@ -1,12 +1,15 @@
+i=0
+total_files=$(find "./envs" -type f | wc -l)
 for file in $PWD/envs/{*,.*}
 do
     if [ -f "$file" ]; then
-      echo "start clear old db $DB_NAME"
+      ((i++))
+      echo "($i/$total_files) start clear old db $DB_NAME"
       source $file
       DIR="./bkp/$DB_NAME/"
       KEEP=72
       find "$DIR" -type f -printf "%T@ %p\n" | sort -n | head -n -"$KEEP" | cut -d' ' -f2- | xargs -d '\n' rm -f
-      echo "ended clear old db $DB_NAME"
+      echo "($i/$total_files) ended clear old db $DB_NAME"
     fi
 done
 
