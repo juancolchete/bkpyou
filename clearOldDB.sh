57c7ff4870678484a49cabb2afb2de56aca1032e
juancolchete <juanudkp@gmail.com>
2024-07-31 20:00:10 -0300

    chore: indicate progress

--- clearOldDB.sh
+++ clearOldDB.sh
@@ -1,10 +1,12 @@
 for file in $PWD/envs/{*,.*}
 do
     if [ -f "$file" ]; then
+      echo "start clear old db $DB_NAME"
       source $file
       DIR="./bkp/$DB_NAME/"
       KEEP=72
       find "$DIR" -type f -printf "%T@ %p\n" | sort -n | head -n -"$KEEP" | cut -d' ' -f2- | xargs -d '\n' rm -f
+      echo "ended clear old db $DB_NAME"
     fi
 done
 
