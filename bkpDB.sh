09c7ffb4eca8a5b6fa665ba185c8d3fe9204e6e8
juancolchete <juanudkp@gmail.com>
2024-07-31 16:46:42 -0300

    fix: missconfiguration

--- bkpDB.sh
+++ bkpDB.sh
@@ -1,6 +1,5 @@
 source ./.env
 
 mkdir -p ./bkp/$DB_NAME
-for i in $(seq 1 100)
-  do sleep 1 && timestamp=$(date +%s) && mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
-done
+timestamp=$(date +%s)
+mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
