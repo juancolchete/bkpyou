fb8f1abcc7b102cf11d52655bd25b3e7fc07844d
juancolchete <juanudkp@gmail.com>
2024-09-02 17:27:44 -0300

    ci: check if file is lager than one mybite

--- bkpDB.sh
+++ bkpDB.sh
@@ -8,7 +8,11 @@ do
       echo "($i/$total_files) start bkp $DB_NAME"
       mkdir -p ./bkp/$DB_NAME
       timestamp=$(date +%s)
-      mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
+      mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/$timestamp-temp.sql
+      if [ -n "$(find "$filepath" -prune -size +1000000c)" ]; then
+        printf '%s is strictly larger than 1 MB\n' "$filepath"
+        mv ./bkp/$DB_NAME/$timestamp-temp.sql ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
+      fi
       echo "($i/$total_files) ended bkp $DB_NAME"
     fi
 done
