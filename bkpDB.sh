8b07534c8b3cf56eba49c24f97aa72572ccb0d9d
juancolchete <juanudkp@gmail.com>
2024-09-11 23:20:18 -0300

    ci: postgres kind

--- bkpDB.sh
+++ bkpDB.sh
@@ -10,7 +10,11 @@ do
       echo "($i/$total_files) start bkp $DB_NAME"
       mkdir -p ./bkp/$DB_NAME
       timestamp=$(date +%s)
-      mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/temp.sql
+      if [[ $DB_KIND == "postgres" ]]; then
+        PGPASSWORD=$DB_PASS pg_dump -h $DB_HOST > outfile
+      else
+        mysqldump --ssl-verify-server-cert=0 -u $DB_USER -p$DB_PASS -P $PORT -h $DB_HOST $DB_NAME > ./bkp/$DB_NAME/temp.sql
+      fi
       if [ -n "$(find "./bkp/$DB_NAME/temp.sql" -prune -size +200000c)" ]; then
         printf '%s is strictly larger than 1 MB\n' "$filepath"
         mv ./bkp/$DB_NAME/temp.sql ./bkp/$DB_NAME/$timestamp-$DB_NAME.sql
